031ad61dae1f241aee0c553cfaafa27c66e2a35c
Qinqiang Xu <qinqiangxu@microsoft.com>
2026-04-16 23:04:15 +0800

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QIcon>
+#include <QSharedMemory>
 #include <QtWebEngineWidgets/QWebEngineView>
 #include "app/MainWindow.h"
 #include "db/DatabaseManager.h"
@@ -11,6 +12,12 @@ int main(int argc, char *argv[])
     app.setApplicationVersion("1.0.0");
     app.setOrganizationName("Nexus");
 
+    // Single-instance guard: prevent multiple Nexus processes
+    QSharedMemory singleInstanceGuard("NexusTaskManagerSingleInstance");
+    if (!singleInstanceGuard.create(1)) {
+        // Another instance is already running
+        return 0;
+    }
 
     // Don't quit when last window is closed (we minimize to tray)
     app.setQuitOnLastWindowClosed(false);
@@ -21,6 +28,8 @@ int main(int argc, char *argv[])
     // Create and show main window
     MainWindow mainWindow;
     mainWindow.show();
+    mainWindow.raise();
+    mainWindow.activateWindow();
 
     return app.exec();
 }
